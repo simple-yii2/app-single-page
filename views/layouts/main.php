849c66cc794f2cd8ca04ded5ea09a5b57f0d5d47
Dmitry Khlystov <dkhlystov@gmail.com>
2016-12-29 14:46:35 +1000

    refactoring

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,9 +4,6 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
